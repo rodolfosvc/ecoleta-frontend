48e1a0a8a5c554cd49936b3460b9978a1be9bb7a
Rodolfo Santos <larodolfo.santos@Rodolfos-MacBook-Pro.local>
2020-06-04 23:07:13 -0300

    'Creating point' screen

---
+++ src/pages/CreatePoint/index.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import './styles.css';
+import logo from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+const CreatePoint = () => {
+    return (
+        <div id="page-create-point">
+            <header>
+                <img src={logo} alt="Ecoleta"/>
+                <Link to="/">
+                    <FiArrowLeft/>
+                    Voltar para Home 
+                </Link>
+            </header>
+            <form action="">
+                <h1>Cadastro do <br/> ponto de coleta</h1>
+                <fieldset>
+                    <legend>
+                        <h2>Dados</h2>
+                    </legend>
+                    <div className="field">
+                        <label htmlFor="name">Nome da entidade</label>
+                        <input 
+                            type="text"
+                            name="name"
+                            id="name"/>
+                        <div className="field-group">
+                            <div className="field">
+                                <label htmlFor="email">E-mail</label>
+                                <input 
+                                    type="email"
+                                    name="email"
+                                    id="email"/>
+                            </div>
+                            <div className="field">
+                                <label htmlFor="whatsapp">Whatsapp</label>
+                                <input 
+                                    type="text"
+                                    name="whatsapp"
+                                    id="whatsapp"/>
+                            </div>
+                        </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Endereço</h2>
+                        <span>Selecione o endereço no mapa</span>
+                    </legend>
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="uf">Estado (UF)</label>
+                            <select name="uf" id="uf">
+                                <option value="0">Selecione uma UF</option>
+                            </select>
+                        </div> 
+                        <div className="field">
+                            <label htmlFor="city">Cidade</label>
+                            <select name="city" id="city">
+                                <option value="0">Selecione uma cidade</option>
+                            </select>
+                        </div>        
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Ítens de coleta</h2>
+                        <span>Selecione um ou mais ítens abaixo</span>
+                    </legend>
+                    <ul className="items-grid">
+                        <li>
+                            <img src="" alt=""/>
+                        </li>
+                    </ul>
+                </fieldset>
+                <button type="submit">Cadastrar ponto de coleta</button>
+            </form>
+        </div>
+    )
+}
+
+export default CreatePoint;

c6a6c2b3b0db7e69c49c1b5d6bc627166ac96f81
Rodolfo Santos <larodolfo.santos@Rodolfos-MacBook-Pro.local>
2020-06-04 23:41:37 -0300

    Getting items from server

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.css';
 import logo from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api';
+
+interface Item{
+    id: number,
+    title: string,
+    image_url: string
+}
 
 const CreatePoint = () => {
+
+    const [items, setItems] = useState<Item[]>([]);
+
+    useEffect(() => {
+        api.get('items')
+            .then((response) => {
+                setItems(response.data)
+            })
+    }, [])
+
     return (
         <div id="page-create-point">
             <header>
@@ -80,9 +97,13 @@ const CreatePoint = () => {
                         <span>Selecione um ou mais ítens abaixo</span>
                     </legend>
                     <ul className="items-grid">
-                        <li>
-                            <img src="" alt=""/>
-                        </li>
+                        {items.map(item => (
+                            <li key={item.id}>
+                                <img src={item.image_url} alt={item.title}/>
+                                <span>{item.title}</span>
+                            </li>
+                        ))}
+                        
                     </ul>
                 </fieldset>
                 <button type="submit">Cadastrar ponto de coleta</button>
